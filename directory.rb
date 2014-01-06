dd71c5a1040655119bf3e0abd69c43be4fa48255
Jorja Tracy Hung <jorjahung@gmail.com>
2014-01-06 17:42:02 +0000

    Added student count

--- directory.rb
+++ directory.rb
@@ -1,3 +1,7 @@
 puts "The students of my cohort at Makers Academy"
 puts "-------------------------------------------"
 puts "Jorja Hung"
+
+print "Overall, we have "
+print 9
+puts " great students"
