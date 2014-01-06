854376cb4802da6c729e7fdd7db3a37dd0e94ab0
Jorja Tracy Hung <jorjahung@gmail.com>
2014-01-06 17:43:45 +0000

    Added comments

--- directory.rb
+++ directory.rb
@@ -1,7 +1,9 @@
+# first, we print the list of students
 puts "The students of my cohort at Makers Academy"
 puts "-------------------------------------------"
 puts "Jorja Hung"
-
+# finally, we print the total
 print "Overall, we have "
+# it's important that print() doesn't add new line characters
 print 9
 puts " great students"
