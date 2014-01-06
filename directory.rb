8dd8a597034fa162f95574f70def02e90a26a031
Jorja Tracy Hung <jorjahung@gmail.com>
2014-01-06 17:46:12 +0000

    Added student_count variable and interpolation

--- directory.rb
+++ directory.rb
@@ -1,9 +1,7 @@
+student_count = 1
 # first, we print the list of students
 puts "The students of my cohort at Makers Academy"
 puts "-------------------------------------------"
 puts "Jorja Hung"
 # finally, we print the total
-print "Overall, we have "
-# it's important that print() doesn't add new line characters
-print 9
-puts " great students"
+puts "Overall, we have #{student_count} great students."
