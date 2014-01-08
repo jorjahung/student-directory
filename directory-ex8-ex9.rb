ee069fc296f8f5d1159354ce122117a710fb0895
Jorja Tracy Hung <jorjahung@gmail.com>
2014-01-08 17:03:27 +0000

    Added cohort filtering

--- directory-ex8-ex9.rb
+++ directory-ex8-ex9.rb
@@ -71,12 +71,7 @@ def input_students
 			students << {:name => name, :cohort => cohort}
 
 		end
-
-
-
 	end
-	
-	# ask for the name again
 
 	# return the array of students
 	students.sort {|x,y| x[:cohort] <=> y[:cohort]} 
@@ -87,12 +82,64 @@ def print_footer(names)
 	if names.length == 1
 		puts "Overall, we have #{names.length} great student.".center(100)
 	else
-		puts "Overall, we have #{names.length} great student.".center(100)
+		puts "Overall, we have #{names.length} great students.".center(100)
 	end
 end
 
+# def print_options
+# 	puts "Do you want to 1.) Print all students? or 2.) Print by cohorts? Please choose '1' or '2'"
+# 	@option = gets.to_i
+# end
+
+def print_cohorts(students)
+	puts "Which cohort would you like to see? Please select the month in numerical format."
+	cohort_option = gets.to_i
+		case cohort_option
+			when 1
+				cohort_option = :January
+			when 2
+				cohort_option = :February
+			when 3
+				cohort_option = :March
+			when 4
+				cohort_option = :April
+			when 5
+				cohort_option = :May
+			when 6
+				cohort_option = :June
+			when 7
+				cohort_option = :July
+			when 8
+				cohort_option = :August
+			when 9
+				cohort_option = :September
+			when 10
+				cohort_option = :October
+			when 11
+				cohort_option = :November
+			when 12
+				cohort_option = :December
+			else
+				puts "Error, please input date in numerical format:"
+				cohort = Integer(gets.chomp)
+		end
+
+	puts "The students of the #{cohort_option} cohort are:"
+		students.select{|student| student[:cohort] == cohort_option}.each_with_index do |student,i|
+		puts "#{i+1}. #{student[:name]} -- (#{student[:cohort]} cohort)".center(100)
+		end
+end
 
 students = input_students
-print_header(students)
-print(students)
-print_footer(students)
+puts "Do you want to 1.) Print all students? or 2.) Print by cohorts? Please choose '1' or '2'"
+option = gets.to_i
+	if option == 1
+		print_header(students)
+		print(students)
+		print_footer(students)
+	elsif option == 2
+		print_cohorts(students)
+	else
+		puts "Invalid choice. Please choose between 1 or 2."
+		option.gets.to_i
+	end
