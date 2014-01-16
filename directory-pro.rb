70cea8b3a74c2d7328b0a6879dbbc8ce05c5ddf8
Jorja Hung <jorjahung@gmail.com>
2014-01-16 15:33:08 +0000

    Manual turning back of time because I didn't know what branching was

---
+++ directory-pro.rb
@@ -0,0 +1,217 @@
+# Exercise #8 and 9
+
+@students = []
+
+def print_header(names)
+        if names.length <1
+                puts "There are no students.".center(100)
+        elsif        names.length == 1
+                puts "The only student of Makers Academy".center(100)
+                puts "----------------------------------".center(100)
+        else
+                puts "The students of Makers Academy".center(100)
+                puts "------------------------------".center(100)
+        end
+        
+end
+
+def print_students_list(students)
+# this is called iteration
+        students.each_with_index do |student,i|
+                puts "#{i+1}. #{student[:name]} -- (#{student[:cohort]} cohort)".center(100)
+        end
+end
+
+def input_students
+        puts "Please enter the names of the students."
+        puts "To finish, just hit return twice."
+        # @students = []
+        
+        #get the first name, but first create a variable so that it exists
+        name = nil
+
+        while name.nil? || !name.empty? do
+
+                puts "First name:"
+                name = STDIN.gets.chomp
+                name.capitalize!
+
+                months = [:January,:February,:March,:April,:May,:June,:July,:August,:September,:October,:November,:December]
+                cohort = nil
+                if !name.empty? 
+                
+                        loop do
+                                puts "Cohort (in numerical format):"
+                                cohort = STDIN.gets.to_i
+                                unless (1..12).include?(cohort)
+                                        puts "Please try again"
+                                        redo
+                                end
+                                cohort = months[cohort-1]
+                                break
+                        end
+                        
+                        # puts the input into the array
+                        @students << {:name => name, :cohort => cohort}
+                end
+        end
+
+        
+        months = [:January,:February,:March,:April,:May,:June,:July,:August,:September,:October,:November,:December]
+        
+        # sorts the array of students by cohort
+        @students.sort {|x,y| months.index(x[:cohort]) <=> months.index(y[:cohort])} 
+        
+        # sorts the array by name
+        # students.sort {|x,y| x[:name] <=> y[:name] } 
+
+
+end
+
+
+def print_footer(names)
+        if names.length <1
+                puts "No students enrolled.".center(100)
+        elsif names.length == 1
+                puts "Overall, we have #{names.length} great student.".center(100)
+        else
+                puts "Overall, we have #{names.length} great students.".center(100)
+        end
+end
+
+
+def print_cohorts(students)
+
+
+        months = [:January,:February,:March,:April,:May,:June,:July,:August,:September,:October,:November,:December]
+        cohort = nil
+                        loop do
+                                puts "Which cohort would you like to see? Please select the month in numerical format."
+                                cohort = STDIN.gets.to_i
+                                unless (1..12).include?(cohort)
+                                        puts "Please try again"
+                                        redo
+                                end
+                                cohort = months[cohort-1]
+                                break
+                        end        
+                        
+# This is how you select students of a particular cohort
+        if         @students.length == 0
+                puts "There are no students enrolled in that cohort."
+
+        elsif @students.count > 1        
+                puts "The students of the #{cohort} cohort are:".center(100)
+                @students.select{|student| student[:cohort] == cohort}.each_with_index do |student,i|
+                puts "#{i+1}. #{student[:name]} -- (#{student[:cohort]} cohort)".center(100)
+                end
+
+        elsif @students.count == 1
+                puts "The only student of the #{cohort} cohort is:".center(100)
+                @students.select{|student| student[:cohort] == cohort}.each_with_index do |student,i|
+                puts "#{i+1}. #{student[:name]} -- (#{student[:cohort]} cohort)".center(100)
+                end
+
+        end
+end
+
+def print_students
+        puts "Do you want to 1.) Print all students? or 2.) Print by cohorts? Please choose '1' or '2'"
+        option = STDIN.gets.to_i
+        if option == 1
+                print_header(@students)
+                print_students_list(@students)
+                print_footer(@students)
+                interactive_menu
+        elsif option == 2
+                print_cohorts(@students)
+                interactive_menu
+        else
+                puts "Invalid choice. Please choose between 1 or 2."
+                option.STDIN.gets.to_i
+        end
+end
+
+def interactive_menu
+        loop do
+        
+        print_menu
+        process(STDIN.gets.to_i)
+
+        end
+end
+
+def print_menu
+        puts "What do you want to do next?"
+        puts "1.) Input more students" 
+        puts "2.) Print names"
+        puts "3.) Remove a name"
+        puts "4.) Save the list to students.csv"
+        puts "9.) Exit"
+end
+
+def process(selection)
+        case selection
+                when 1
+                        input_students
+                when 2
+                        print_students
+                when 3
+                        remove_name
+                when 4
+                        save_students        
+                when 9
+                        exit
+                else
+                        puts "I don't know what you mean, please try again."
+                        print_menu
+                end
+end                
+
+def save_students
+        # open the file for writing
+        file = File.open("students.csv", "w")
+        # iterate over the array of students
+        @students.each do |student|
+                student_data = [student[:name], student[:cohort]]
+                csv_line = student_data.join(",")
+                file.puts csv_line
+        end
+# delete??
+        file.close
+end        
+
+def load_students(filename = "students.csv")
+        file = File.open(filename, "r")
+        file.readlines.each do |line|
+                name, cohort = line.chomp.split (',')
+                @students << {name: name, cohort: cohort.to_sym}
+        end
+        file.close
+end        
+
+def try_load_students
+        filename = ARGV.first
+        if filename.nil?
+                load_students
+        elsif File.exists?(filename)
+                load_students(filename)
+                puts "Loaded #{@students.length} from #{filename}"
+        else
+                puts "Sorry, #{filename} does not exist."
+                exit
+        end
+end
+
+def remove_name
+        puts "Which name do you want to delete?"
+        puts "Please select the number"
+        print_students_list(@students)
+        print "Delete: "
+        name = gets.to_i - 1
+        @students.delete_at(name)
+end
+
+try_load_students
+@students = input_students
+interactive_menu
--- directory.rb
+++ directory.rb
@@ -1,215 +1,88 @@
-# Exercise #8 and 9
+# # let's put all students and their cohorts into an array
+# students = [
+# 	{:name => "Jorja", :cohort => :january},
+# 	{:name => "Berta", :cohort => :january},
+# 	{:name => "Andrey", :cohort => :january},
+# 	{:name => "Peter", :cohort => :january},
+# 	{:name => "James", :cohort => :january},
+# 	{:name => "Megan", :cohort => :january},
+# 	{:name => "Rolando", :cohort => :january},
+# 	{:name => "Biwek", :cohort => :january},
+# 	{:name => "Colin", :cohort => :january},
+# 	{:name => "Kalle", :cohort => :january},
+# 	{:name => "Makis", :cohort => :january},
+# 	{:name => "Ayaz", :cohort => :january},
+# 	{:name => "Roy", :cohort => :january},
+# 	{:name => "Mihai", :cohort => :january},
+# 	{:name => "Nabin", :cohort => :january},
+# 	{:name => "Abraham", :cohort => :january},
+# ]
 
-@students = []
-require 'date'
+
+# let's create methods
 
 def print_header(names)
-	if names.length <1
-		puts "There are no students.".center(100)
-	elsif	names.length == 1
-		puts "The only student of Makers Academy".center(100)
-		puts "----------------------------------".center(100)
-	else
-		puts "The students of Makers Academy".center(100)
-		puts "------------------------------".center(100)
+	if names.length == 1
+		puts "The only student of my cohort at Makers Academy".center(100)
+		puts "-----------------------------------------------".center(100)
+	else 
+		puts "The students of my cohort at Makers Academy".center(100)
+		puts "-------------------------------------------".center(100)
 	end
-	
 end
 
-def print_students_list(students)
-	students.each_with_index {|student,i| puts "#{i+1}. #{student[:name]} -- (#{student[:cohort]} cohort)".center(100)}
+def print(students)
+# this is called iteration
+	students.each_with_index do |student,i|
+		puts "#{i+1}. #{student[:name]} (#{student[:cohort]} cohort)".center(100)
+	end
+
+# Exercise #5 (?) answer:
+# 	student_count = students.count
+# while students.count > 0
+# 	puts "#{student_count-students.count+1}. #{students.first[:name]} (#{students.first[:cohort]} cohort)".center(100)
+# 	students.shift
+# end
+
+end
+
+def print_footer(names)
+	if names.length == 1
+		puts "Overall, we have #{names.length} great student.".center(100)
+	else
+		puts "Overall, we have #{names.length} great student.".center(100)
+	end
 end
 
 def input_students
 	puts "Please enter the names of the students."
 	puts "To finish, just hit return twice."
-	
-	#get the first name, but first create a variable so that it exists
-	name = nil
-	months = Date::MONTHNAMES
-	months = months.map { |month| month.nil? ? "" : month.to_sym}
-	# [:January,:February,:March,:April,:May,:June,:July,:August,:September,:October,:November,:December]
-
-	while name.nil? || !name.empty? do
-
-		puts "First name:"
-		name = STDIN.gets.chomp
-		name.capitalize!
-
-		cohort = nil
-		if !name.empty? 
-		
-			loop do
-				puts "Cohort (in numerical format):"
-				cohort = STDIN.gets.to_i
-				unless (1..12).include?(cohort)
-					puts "Please try again"
-					redo
-				end
-				cohort =  Date::MONTHNAMES[cohort].to_sym
-				break
-			end
-			
-			# puts the input into the array
-			@students << {:name => name, :cohort => cohort}
-		end
-	end	
-
-	
-	# sorts the array of students by cohort
-	@students.sort {|x,y|  months.index(x[:cohort]) <=>  months.index(y[:cohort])} 
-	
-	# sorts the array by name
-	# students.sort {|x,y| x[:name] <=> y[:name] } 
-
-
-end
-
-
-def print_footer(names)
-	if names.length <1
-		puts "No students enrolled.".center(100)
-	elsif names.length == 1
-		puts "Overall, we have #{names.length} great student.".center(100)
-	else
-		puts "Overall, we have #{names.length} great students.".center(100)
-	end
-end
-
-
-def print_cohorts(students)
-
-
-	 # Date::MONTHNAMES = [:January,:February,:March,:April,:May,:June,:July,:August,:September,:October,:November,:December]
-	cohort = nil
-			loop do
-				puts "Which cohort would you like to see? Please select the month in numerical format."
-				cohort = STDIN.gets.to_i
-				unless (1..12).include?(cohort)
-					puts "Please try again"
-					redo
-				end
-				cohort =  Date::MONTHNAMES[cohort]
-				break
-			end	
-			
-# This is how you select students of a particular cohort
-	if 	@students.length == 0
-		puts "There are no students enrolled in that cohort."
-
-	elsif @students.count > 1	
-		puts "The students of the #{cohort} cohort are:".center(100)
-		@students.select{|student| student[:cohort] == cohort}.each_with_index do |student,i|
-		puts "#{i+1}. #{student[:name]} -- (#{student[:cohort]} cohort)".center(100)
-		end
-
-	elsif @students.count == 1
-		puts "The only student of the #{cohort} cohort is:".center(100)
-		@students.select{|student| student[:cohort] == cohort}.each_with_index do |student,i|
-		puts "#{i+1}. #{student[:name]} -- (#{student[:cohort]} cohort)".center(100)
-		end
-
-	end
-end
-
-def print_students
-	puts "Do you want to 1.) Print all students? or 2.) Print by cohorts? Please choose '1' or '2'"
-	option = STDIN.gets.to_i
-	if option == 1
-		print_header(@students)
-		print_students_list(@students)
-		print_footer(@students)
-		interactive_menu
-	elsif option == 2
-		print_cohorts(@students)
-		interactive_menu
-	else
-		puts "Invalid choice. Please choose between 1 or 2."
-		option.STDIN.gets.to_i
-	end
-end
-
-def interactive_menu
-	loop do
-	
-	print_menu
-	process(STDIN.gets.to_i)
-
-	end
-end
-
-def print_menu
-	puts "What do you want to do next?"
-	puts "1.) Input more students" 
-	puts "2.) Print names"
-	puts "3.) Remove a name"
-	puts "4.) Save the list to students.csv"
-	puts "9.) Exit"
-end
-
-def process(selection)
-	case selection
-		when 1
-			input_students
-		when 2
-			print_students
-		when 3
-			remove_name
-		when 4
-			save_students	
-		when 9
-			exit
+	# create an empty array
+	students = []
+	#get the first name
+	name = gets.chomp
+	name.capitalize!
+	# while the name is not empty, repeat this code
+	while !name.empty? do
+		#add the student hash to the array
+		students << {:name => name, :cohort => :january}
+		if students.length == 1
+			puts "Now we have #{students.length} student."
 		else
-			puts "I don't know what you mean, please try again."
-			print_menu
+			puts "Now we have #{students.length} students."
 		end
-end		
-
-def save_students
-	# open the file for writing
-	file = File.open("students.csv", "w")
-	# iterate over the array of students
-	@students.each do |student|
-		student_data = [student[:name], student[:cohort]]
-		csv_line = student_data.join(",")
-		file.puts csv_line
+		#get another name from the user
+		name = gets.chomp
+		name.capitalize!
 	end
-# delete??
-	file.close
-end	
-
-def load_students(filename = "students.csv")
-	file = File.open(filename, "r")
-	file.readlines.each do |line|
-		name, cohort = line.chomp.split (',')
-		@students << {name: name, cohort: cohort.to_sym}
-	end
-	file.close
-end	
-
-def try_load_students
-	filename = ARGV.first
-	if filename.nil?
-		load_students
-	elsif File.exists?(filename)
-		load_students(filename)
-		puts "Loaded #{@students.length} from #{filename}"
-	else
-		puts "Sorry, #{filename} does not exist."
-		exit
-	end
+	# return the array of students
+	students.sort {|x,y| x[:name] <=> y[:name] } 
 end
+# end of methods we created
 
-def remove_name
-	puts "Which name do you want to delete?"
-	puts "Please select the number"
-	print_students_list(@students)
-	print "Delete: "
-	name = gets.to_i - 1
-	@students.delete_at(name)
-end
+# nothing happens until we call the methods
 
-try_load_students
-@students = input_students
-interactive_menu
-
+students = input_students
+print_header(students)
+print(students)
+print_footer(students)
